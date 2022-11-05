e660c2cfd02c248b524c10d567d9429250524e9b
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2022-11-06 00:57:26 +0100

    Fix base backup

--- src/storage/clickHouse/clickHouseStorage.ts
+++ src/storage/clickHouse/clickHouseStorage.ts
@@ -65,7 +65,7 @@ export class ClickHouseStorage implements DataStorage {
 
     if (process.env.BACKUP_S3_ENABLE === '1') {
       this.clickHouse.exec({
-        query: `BACKUP DATABASE ${process.env.CLICKHOUSE_NAME} TO S3('${process.env.BACKUP_S3_URL}/base', '${process.env.BACKUP_S3_ACCESS_KEY}', '${process.env.BACKUP_S3_SECRET_KEY}') SETTINGS base_backup = S3('${process.env.BACKUP_S3_URL}/incremental', '${process.env.BACKUP_S3_URL}', '${process.env.BACKUP_S3_SECRET_KEY}');`,
+        query: `BACKUP DATABASE ${process.env.CLICKHOUSE_NAME} TO S3('${process.env.BACKUP_S3_URL}/base', '${process.env.BACKUP_S3_ACCESS_KEY}', '${process.env.BACKUP_S3_SECRET_KEY}') SETTINGS base_backup = S3('${process.env.BACKUP_S3_URL}/incremental', '${process.env.BACKUP_S3_ACCESS_KEY}', '${process.env.BACKUP_S3_SECRET_KEY}');`,
       }).catch((err) => {
         console.error('Attempt to run backup failed', err);
       });
